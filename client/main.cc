288a759592f9b0ae460e314aa15d8ea03b6d77a7
untodesu <41549137+untodesu@users.noreply.github.com>
2023-10-11 19:17:27 +0500

    Politely ask Windows to cut the shit

--- client/main.cc
+++ client/main.cc
@@ -16,6 +16,11 @@
 #include <shared/epoch.hh>
 #include <spdlog/spdlog.h>
 
+#if defined(_WIN32)
+extern "C" __declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
+extern "C" __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+#endif
+
 static void on_glfw_error(int code, const char *message)
 {
     spdlog::error("glfw: {}", message);
