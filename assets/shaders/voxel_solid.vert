ccf3dd4d22fa301cb58230d752d6da5d852f27d0
untodesu <41549137+untodesu@users.noreply.github.com>
2024-03-03 20:44:14 +0500

    Re-license to GPLv2

--- assets/shaders/voxel_solid.vert
+++ assets/shaders/voxel_solid.vert
@@ -1,7 +1,5 @@
-// SPDX-License-Identifier: MPL-2.0
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// SPDX-License-Identifier: GPL-2.0-only
+// Copyright (c) 2024, Voxelius Contributors
 #version 450 core
 #pragma import common
 #pragma import voxel_anims
--- client/debug_session.hh
+++ client/debug_session.hh
@@ -1,7 +1,15 @@
-// SPDX-License-Identifier: MPL-2.0
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// SPDX-License-Identifier: GPL-2.0-only
+// Copyright (c) 2024, Voxelius Contributors
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 #ifndef CLIENT_DEBUG_SESSION_HH
 #define CLIENT_DEBUG_SESSION_HH
 
