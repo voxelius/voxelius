eb249027823c7cf386b8a051e329fab55a752a64
untodesu <41549137+untodesu@users.noreply.github.com>
2024-07-24 22:38:21 +0500

    Update client/main.cc

--- client/main.cc
+++ client/main.cc
@@ -26,7 +26,6 @@ extern "C" __declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
 extern "C" __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
 #endif
 
-
 static void on_glfw_error(int code, const char *message)
 {
     spdlog::error("glfw: {}", message);
