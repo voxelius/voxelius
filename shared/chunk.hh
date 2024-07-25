e66f738a6c48bb97583b899c99c04c4a2f54d4cb
untodesu <41549137+untodesu@users.noreply.github.com>
2024-07-25 12:34:14 +0500

    Forward-port a mutable (polymorphic) voxel storage

--- shared/chunk.hh
+++ shared/chunk.hh
@@ -4,17 +4,37 @@
 #define SHARED_CHUNK_HH
 #include <array>
 #include <entt/entity/entity.hpp>
-#include <shared/const.hh>
-#include <shared/light.hh>
+#include <shared/coord.hh>
 #include <shared/voxel.hh>
 
-using LightArray = std::array<Light, CHUNK_VOLUME>;
-using VoxelArray = std::array<Voxel, CHUNK_VOLUME>;
+enum class ChunkStorageType {
+    Unallocated = 0xFFFF,
+    SingleFill  = 0x0000,
+    Palette     = 0x0001,
+    Array       = 0x0002,
+};
 
-struct Chunk final {
+class Chunk final {
+public:
     entt::entity entity {};
-    LightArray lightmap {};
-    VoxelArray voxels {};
+    ChunkStorageType storage_type {};
+    void *storage {};
+
+public:
+    static void create_storage(Chunk &chunk);
+    static void create_storage(Chunk &chunk, const Chunk &source);
+    static void destroy_storage(Chunk &chunk);
+
+public:
+    static Voxel get_voxel(const Chunk &chunk, const LocalPos &lpos);
+    static Voxel get_voxel(const Chunk &chunk, const std::size_t index);
+
+public:
+    static void set_voxel(Chunk &chunk, Voxel voxel, const LocalPos &lpos);
+    static void set_voxel(Chunk &chunk, Voxel voxel, const std::size_t index);
+
+public:
+    static void fill(Chunk &chunk, Voxel voxel);
 };
 
 #endif /* SHARED_CHUNK_HH */
--- shared/voxel.hh
+++ shared/voxel.hh
@@ -8,4 +8,8 @@ using Voxel = std::uint16_t;
 constexpr static Voxel VOXEL_MAX = UINT16_MAX;
 constexpr static Voxel NULL_VOXEL = 0x0000;
 
+// A single Voxel must fit inside a void pointer
+// for ChunkStorageType::SingleFill storage to work
+static_assert(sizeof(Voxel) <= sizeof(void *));
+
 #endif /* SHARED_VOXEL_HH */
