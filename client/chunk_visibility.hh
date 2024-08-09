93c7b185ce313cf8993334b46464f384dbd5f13c
untodesu <41549137+untodesu@users.noreply.github.com>
2024-08-09 11:05:02 +0500

    Visibility checker improvements

--- client/chunk_visibility.hh
+++ client/chunk_visibility.hh
@@ -2,8 +2,17 @@
 // Copyright (C) 2024, Voxelius Contributors
 #ifndef CLIENT_CHUNK_VIS_HH
 #define CLIENT_CHUNK_VIS_HH
+#include <entt/entity/entity.hpp>
+#include <shared/coord.hh>
 
 struct ChunkVisibleComponent final {};
+
+namespace chunk_visibility
+{
+void update_chunk(entt::entity entity);
+void update_chunk(const ChunkCoord &cvec);
+void update_chunks(void);
+} // namespace chunk_visibility
 
 namespace chunk_visibility
 {
