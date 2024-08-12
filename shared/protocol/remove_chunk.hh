d333b74457f4151629644a2955523d023dc0defd
untodesu <41549137+untodesu@users.noreply.github.com>
2024-08-12 15:55:00 +0500

    Add the entirety of protocol V2 packet structures It's version 2 because the version 1 resides in the historic repo

---
+++ shared/protocol/remove_chunk.hh
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Zlib
+// Copyright (C) 2024, Voxelius Contributors
+#ifndef SHARED_PROTOCOL_REMOVE_CHUNK_HH
+#define SHARED_PROTOCOL_REMOVE_CHUNK_HH
+#include <shared/protocol/protocol.hh>
+#include <shared/coord.hh>
+
+struct protocol::RemoveChunk final : public protocol::BasePacket<0x000C> {
+    ChunkCoord chunk {};
+};
+
+template<>
+static inline void protocol::decode_packet<protocol::RemoveChunk>(PacketReader &reader, protocol::RemoveChunk &packet)
+{
+    packet.chunk[0] = PacketReader::I32(reader);
+    packet.chunk[1] = PacketReader::I32(reader);
+    packet.chunk[2] = PacketReader::I32(reader);
+}
+
+template<>
+static inline void protocol::encode_packet<protocol::RemoveChunk>(PacketWriter &writer, const protocol::RemoveChunk &packet)
+{
+    PacketWriter::I32(writer, packet.chunk[0]);
+    PacketWriter::I32(writer, packet.chunk[1]);
+    PacketWriter::I32(writer, packet.chunk[2]);
+}
+
+#endif /* SHARED_PROTOCOL_REMOVE_CHUNK_HH */
