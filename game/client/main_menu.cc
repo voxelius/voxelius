6a4caeb01462b0af7203f78ef34601c8afc3c076
untodesu <41549137+untodesu@users.noreply.github.com>
2024-08-28 16:09:40 +0500

    Rename server_list to play_menu

--- game/client/main_menu.cc
+++ game/client/main_menu.cc
@@ -3,19 +3,21 @@
 #include <config/cmake.hh>
 #include <entt/entity/registry.hpp>
 #include <entt/signal/dispatcher.hpp>
+#include <game/client/event/language_set.hh>
 #include <game/client/debug_session.hh>
 #include <game/client/event/glfw_key.hh>
-#include <game/client/event/language_set.hh>
 #include <game/client/globals.hh>
 #include <game/client/gui_screen.hh>
 #include <game/client/main_menu.hh>
+#include <game/client/session.hh>
 #include <imgui.h>
 
 constexpr static ImGuiWindowFlags MENU_FLAGS = ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoDecoration;
 
-static std::string str_debug_session = {};
-static std::string str_server_list = {};
+static std::string str_play = {};
+static std::string str_resume = {};
 static std::string str_settings = {};
+static std::string str_leave = {};
 static std::string str_quit = {};
 
 static void on_glfw_key(const GlfwKeyEvent &event)
@@ -37,9 +39,10 @@ static void on_glfw_key(const GlfwKeyEvent &event)
 
 static void on_language_set(const LanguageSetEvent &event)
 {
-    str_debug_session = language::resolve_ui("main_menu.debug_session");
-    str_server_list = language::resolve("main_menu.server_list");
+    str_play = language::resolve_ui("main_menu.play");
+    str_resume = language::resolve_ui("main_menu.resume");
     str_settings = language::resolve("main_menu.settings");
+    str_leave = language::resolve("main_menu.leave");
     str_quit = language::resolve("main_menu.quit");
 }
 
@@ -83,25 +86,39 @@ void main_menu::layout(void)
         const float button_width = 240.0f * globals::gui_scale;
         const float button_xpos = 0.5f * (window_size.x - button_width);
 
-        ImGui::SetCursorPosX(button_xpos);
-        if(ImGui::Button(str_debug_session.c_str(), ImVec2(button_width, 0.0f)))
-            debug_session::run();
-        ImGui::Spacing();
-
-        ImGui::SetCursorPosX(button_xpos);
-        if(ImGui::Button(str_server_list.c_str(), ImVec2(button_width, 0.0f)))
-            globals::gui_screen = GUI_SERVER_LIST;
-        ImGui::Spacing();
+        if(globals::registry.valid(globals::player)) {
+            ImGui::SetCursorPosX(button_xpos);
+            if(ImGui::Button(str_resume.c_str(), ImVec2(button_width, 0.0f)))
+                globals::gui_screen = GUI_SCREEN_NONE;
+            ImGui::Spacing();
+        }
+        else {
+            ImGui::SetCursorPosX(button_xpos);
+            if(ImGui::Button(str_play.c_str(), ImVec2(button_width, 0.0f)))
+                globals::gui_screen = GUI_PLAY_MENU;
+            ImGui::Spacing();
+        }
 
         ImGui::SetCursorPosX(button_xpos);
         if(ImGui::Button(str_settings.c_str(), ImVec2(button_width, 0.0f)))
             globals::gui_screen = GUI_SETTINGS;
         ImGui::Spacing();
 
-        ImGui::SetCursorPosX(button_xpos);
-        if(ImGui::Button(str_quit.c_str(), ImVec2(button_width, 0.0f)))
-            glfwSetWindowShouldClose(globals::window, true);
-        ImGui::Spacing();
+        if(globals::registry.valid(globals::player)) {
+            ImGui::SetCursorPosX(button_xpos);
+            if(ImGui::Button(str_leave.c_str(), ImVec2(button_width, 0.0f))) {
+                session::disconnect("protocol.client_disconnect");
+                globals::gui_screen = GUI_PLAY_MENU;
+            }
+
+            ImGui::Spacing();
+        }
+        else {
+            ImGui::SetCursorPosX(button_xpos);
+            if(ImGui::Button(str_quit.c_str(), ImVec2(button_width, 0.0f)))
+                glfwSetWindowShouldClose(globals::window, true);
+            ImGui::Spacing();            
+        }
 
         ImGui::PopStyleVar();
         ImGui::SameLine();
