6cbaf958506baafdad7cdbb4f1e8f099f993abd6
untodesu <41549137+untodesu@users.noreply.github.com>
2024-09-01 23:36:33 +0500

    32x32x32 chunks are better for some reason I guess

--- game/shared/const.hh
+++ game/shared/const.hh
@@ -3,7 +3,7 @@
 #pragma once
 #include <mathlib/constexpr.hh>
 
-constexpr static std::size_t CHUNK_SIZE = 16;
+constexpr static std::size_t CHUNK_SIZE = 32;
 constexpr static std::size_t CHUNK_AREA = CHUNK_SIZE * CHUNK_SIZE;
 constexpr static std::size_t CHUNK_VOLUME = CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE;
 constexpr static std::size_t CHUNK_SIZE_LOG2 = cxpr::log2(CHUNK_SIZE);
