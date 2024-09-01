50d26615e57074fb33e736eaab8468db9f7fa335
untodesu <41549137+untodesu@users.noreply.github.com>
2024-09-01 16:37:51 +0500

    Move world generation into a separate source file

--- game/server/game.cc
+++ game/server/game.cc
@@ -14,65 +14,13 @@
 #include <game/shared/game_voxels.hh>
 #include <game/shared/protocol.hh>
 #include <game/shared/splash.hh>
+#include <game/shared/world.hh>
+#include <game/shared/worldgen.hh>
 #include <mathlib/constexpr.hh>
 #include <spdlog/spdlog.h>
 
-// UNDONE: worldgen in shared
-#include <FastNoiseLite.h>
-#include <game/shared/world.hh>
-#include <random>
-
 static unsigned int listen_port = protocol::PORT;
 static unsigned int status_peers = 4U;
-
-// Surface level for world generation
-constexpr static const int64_t SURFACE = 0;
-
-static fnl_state noise = {};
-
-// This is VERY SLOW
-// UNDONE/TODO: move this into server worldgen code
-static Voxel voxel_at(const VoxelCoord &vpos)
-{
-    static std::uniform_int_distribution intdist = std::uniform_int_distribution(-2, +2);
-    static std::mt19937_64 twister = std::mt19937_64(std::random_device()());
-    int64_t surf = SURFACE + 16.0f * fnlGetNoise2D(&noise, vpos[0] / 2.0f, vpos[2] / 2.0f);
-    if(vpos[1] <= surf - 32 + intdist(twister))
-        return game_voxels::slate;
-    if(vpos[1] <= surf - 8 + intdist(twister))
-        return game_voxels::stone;
-    if(vpos[1] <= surf - 1)
-        return game_voxels::dirt;
-    if(vpos[1] <= surf)
-        return game_voxels::grass;
-    return NULL_VOXEL;
-}
-
-static void generate(const ChunkCoord &cpos)
-{
-    spdlog::trace("generating {} {} {}", cpos[0], cpos[1], cpos[2]);
-
-    VoxelStorage voxels = {};
-    bool voxels_dirty = false;
-    
-    for(std::size_t i = 0; i < CHUNK_VOLUME; ++i) {
-
-        const auto lpos = LocalCoord::from_index(i);
-        const auto vpos = ChunkCoord::to_voxel(cpos, lpos);
-        const auto voxel = voxel_at(vpos);
-
-        if(voxel != NULL_VOXEL) {
-            voxels_dirty = true;
-            voxels[i] = voxel;
-            continue;
-        }
-    }
-
-    if(voxels_dirty) {
-        Chunk *chunk = world::assign(cpos, globals::registry.create());
-        chunk->voxels = voxels;
-    }
-}
 
 void server_game::init(void)
 {
@@ -87,6 +35,7 @@ void server_game::init(void)
     server_recieve::init();
 
     world::init();
+    worldgen::init(42);
 }
 
 void server_game::init_late(void)
@@ -112,34 +61,15 @@ void server_game::init_late(void)
 
     game_voxels::populate();
 
-    // Debug
-
-    noise = fnlCreateState();
-    noise.noise_type = FNL_NOISE_OPENSIMPLEX2;
-    noise.fractal_type = FNL_FRACTAL_RIDGED;
-
-    constexpr int WSIZE = 8;
-    for(int x = -WSIZE; x < WSIZE; x += 1)
-    for(int z = -WSIZE; z < WSIZE; z += 1)
-    for(int y = -4; y < 1; y += 1) {
-        generate({x, y, z});
+    constexpr int WSIZE = 16;
+    for(int x = -WSIZE; x < WSIZE; x += 1) {
+        for(int z = -WSIZE; z < WSIZE; z += 1) {
+            for(int y = -4; y < 3; y += 1) {
+                spdlog::info("generating {} {} {}", x, y, z);
+                worldgen::generate(ChunkCoord(x, y, z));
+            }
+        }
     }
-
-    constexpr int DWSIZE = 2 * WSIZE;
-    for(int x = -DWSIZE; x < DWSIZE; ++x)
-    for(int z = -DWSIZE; z < DWSIZE; ++z) {
-        const auto pos = ChunkCoord(x, -3, z);
-        Chunk *chunk = world::find(pos);
-        if(!chunk)
-            chunk = world::assign(pos, globals::registry.create());
-        chunk->voxels.fill(game_voxels::stone);
-    }
-
-    const auto pos = ChunkCoord(0, 4, 0);
-    Chunk *chunk = world::find(pos);
-    if(!chunk)
-        chunk = world::assign(pos, globals::registry.create());
-    chunk->voxels.fill(game_voxels::vtest);
 }
 
 void server_game::deinit(void)
