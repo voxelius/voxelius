93c2df3e77f8dd4653bd3d8875b74702181d2ee9
untodesu <41549137+untodesu@users.noreply.github.com>
2024-09-04 10:26:07 +0500

    As it turns out 32x32x32 chunks are very annoying This reverts commit 6cbaf958506baafdad7cdbb4f1e8f099f993abd6.

--- game/client/chunk_quad.hh
+++ game/client/chunk_quad.hh
@@ -7,8 +7,8 @@
 #include <mathlib/vec3f.hh>
 #include <utility>
 
-// [0] XXXXXXXXXXYYYYYYYYYYZZZZZZZZZZ--
-// [1] WWWWWHHHHHFFFFTTTTTTTTTTTAAAAA--
+// [0] XXXXXXXXYYYYYYYYZZZZZZZZWWWWHHHH
+// [1] FFFFTTTTTTTTTTTAAAAA------------
 using ChunkQuad = std::array<std::uint32_t, 2>;
 
 constexpr inline static ChunkQuad make_chunk_quad(const Vec3f &position, const Vec2f &size, VoxelFacing facing, std::size_t texture, std::size_t frames)
@@ -17,21 +17,21 @@ constexpr inline static ChunkQuad make_chunk_quad(const Vec3f &position, const V
     result[0] = 0x00000000;
     result[1] = 0x00000000;
 
-    // [0] XXXXXXXXXXYYYYYYYYYYZZZZZZZZZZ--
-    result[0] |= (0x000003FFU & static_cast<std::uint32_t>(position[0] * 32.0f)) << 22U;
-    result[0] |= (0x000003FFU & static_cast<std::uint32_t>(position[1] * 32.0f)) << 12U;
-    result[0] |= (0x000003FFU & static_cast<std::uint32_t>(position[2] * 32.0f)) << 2U;
+    // [0] XXXXXXXXYYYYYYYYZZZZZZZZ--------
+    result[0] |= (0x000000FFU & static_cast<std::uint32_t>(position[0] * 16.0f)) << 24U;
+    result[0] |= (0x000000FFU & static_cast<std::uint32_t>(position[1] * 16.0f)) << 16U;
+    result[0] |= (0x000000FFU & static_cast<std::uint32_t>(position[2] * 16.0f)) << 8U;
 
-    // [1] WWWWWHHHHH----------------------
-    result[1] |= (0x0000001FU & static_cast<std::uint32_t>(size[0] * 32.0f - 1.0f)) << 27U;
-    result[1] |= (0x0000001FU & static_cast<std::uint32_t>(size[1] * 32.0f - 1.0f)) << 22U;
+    // [0] ------------------------WWWWHHHH
+    result[0] |= (0x0000000FU & static_cast<std::uint32_t>(size[0] * 16.0f - 1.0f)) << 4U;
+    result[0] |= (0x0000000FU & static_cast<std::uint32_t>(size[1] * 16.0f - 1.0f));
 
-    // [1] ----------FFFF------------------
-    result[1] |= (0x0000000FU & static_cast<std::uint32_t>(facing)) << 18U;
+    // [1] FFFF----------------------------
+    result[1] |= (0x0000000FU & static_cast<std::uint32_t>(facing)) << 28U;
 
-    // [1] --------------TTTTTTTTTTTAAAAA--
-    result[1] |= (0x000007FFU & static_cast<std::uint32_t>(texture)) << 7U;
-    result[1] |= (0x0000001FU & static_cast<std::uint32_t>(frames)) << 2U;
+    // [1] ----TTTTTTTTTTTAAAAA------------
+    result[1] |= (0x000007FFU & static_cast<std::uint32_t>(texture)) << 17U;
+    result[1] |= (0x0000001FU & static_cast<std::uint32_t>(frames)) << 12U;
 
     return std::move(result);
 }
