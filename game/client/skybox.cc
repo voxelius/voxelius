1bec5abda3c8e5f22e5e790d0a8433ce27cc6486
untodesu <41549137+untodesu@users.noreply.github.com>
2024-09-08 16:37:22 +0500

    Final atmosphere color tweaks - hardcoded now

--- game/client/skybox.cc
+++ game/client/skybox.cc
@@ -9,20 +9,15 @@
 #include <mathlib/vec4f.hh>
 #include <spdlog/spdlog.h>
 
-Vec3f skybox::atmosphere = Vec3f(30.0f, 10.0f, 10.0f);
-Vec2f skybox::sun_angles = Vec2f(0.0f, 0.0f);
-Vec2f skybox::sun_disk = Vec2f(0.025f, 0.035f);
 Vec2f skybox::horizon = Vec2f(0.100f, 0.250f);
-
+Vec2f skybox::sun_angles = Vec2f(0.0f, 0.0f);
 Vec3f skybox::sun_direction = Vec3f(0.0f, 0.0f, 0.0f);
 Vec3f skybox::fog_color = Vec3f(0.00f, 0.00f, 0.00f);
 
 static VariedProgram program = {};
 static std::size_t u_vproj_matrix = {};
 static std::size_t u_world_position = {};
-static std::size_t u_atmosphere = {};
-static std::size_t u_sun_direction = {};
-static std::size_t u_sun_disk = {};
+static std::size_t u_direction = {};
 static std::size_t u_horizon = {};
 
 static GLuint cube_vaobj = {};
@@ -37,9 +32,7 @@ void skybox::init(void)
 
     u_vproj_matrix = VariedProgram::add_uniform(program, "u_ViewProjMatrix");
     u_world_position = VariedProgram::add_uniform(program, "u_WorldPosition");
-    u_atmosphere = VariedProgram::add_uniform(program, "u_Atmosphere");
-    u_sun_direction = VariedProgram::add_uniform(program, "u_SunDirection");
-    u_sun_disk = VariedProgram::add_uniform(program, "u_SunDisk");
+    u_direction = VariedProgram::add_uniform(program, "u_Direction");
     u_horizon = VariedProgram::add_uniform(program, "u_Horizon");
 
     // https://stackoverflow.com/questions/28375338/cube-using-single-gl-triangle-strip
@@ -90,9 +83,9 @@ void skybox::update(void)
     skybox::sun_direction[1] = el_sin;
     skybox::sun_direction[2] = az_cos * el_cos;
 
-    skybox::fog_color[0] = cxpr::clamp(1.0f - std::pow(2.0f, (0.125f + skybox::sun_direction[1]) * (-skybox::atmosphere[0])), 0.0f, 1.0f);
-    skybox::fog_color[1] = cxpr::clamp(1.0f - std::pow(2.0f, (0.125f + skybox::sun_direction[1]) * (-skybox::atmosphere[1])), 0.0f, 1.0f);
-    skybox::fog_color[2] = cxpr::clamp(1.0f - std::pow(2.0f, (0.125f + skybox::sun_direction[1]) * (-skybox::atmosphere[2])), 0.0f, 1.0f);
+    skybox::fog_color[0] = cxpr::clamp(1.0f - std::pow(2.0f, (0.25f + skybox::sun_direction[1]) * (-30.0f)), 0.0f, 1.0f);
+    skybox::fog_color[1] = cxpr::clamp(1.0f - std::pow(2.0f, (0.25f + skybox::sun_direction[1]) * (-10.0f)), 0.0f, 1.0f);
+    skybox::fog_color[2] = cxpr::clamp(1.0f - std::pow(2.0f, (0.25f + skybox::sun_direction[1]) * (-10.0f)), 0.0f, 1.0f);
 }
 
 void skybox::render(void)
@@ -114,9 +107,7 @@ void skybox::render(void)
     glUseProgram(program.handle);
     glUniformMatrix4fv(program.uniforms[u_vproj_matrix].location, 1, false, view::matrix.data()->data());
     glUniform3fv(program.uniforms[u_world_position].location, 1, view::position.local.data());
-    glUniform3fv(program.uniforms[u_atmosphere].location, 1, skybox::atmosphere.data());
-    glUniform3fv(program.uniforms[u_sun_direction].location, 1, skybox::sun_direction.data());
-    glUniform2fv(program.uniforms[u_sun_disk].location, 1, skybox::sun_disk.data());
+    glUniform3fv(program.uniforms[u_direction].location, 1, skybox::sun_direction.data());
     glUniform2fv(program.uniforms[u_horizon].location, 1, skybox::horizon.data());
 
     glBindVertexArray(cube_vaobj);
@@ -127,11 +118,6 @@ void skybox::layout_debug(void)
 {
     ImGui::SliderFloat("azimuth", &skybox::sun_angles[0], 0.0f, 360.0f, "%.0f");
     ImGui::SliderFloat("elevation", &skybox::sun_angles[1], -180.0f, 180.0f, "%.0f");
-    ImGui::SliderFloat("atmosphere[0]", &skybox::atmosphere[0], 0.0, 100.0f, "%.0f");
-    ImGui::SliderFloat("atmosphere[1]", &skybox::atmosphere[1], 0.0, 100.0f, "%.0f");
-    ImGui::SliderFloat("atmosphere[2]", &skybox::atmosphere[2], 0.0, 100.0f, "%.0f");
-    ImGui::SliderFloat("sun_disk[0]", &skybox::sun_disk[0], 0.000f, 0.100f, "%.03f");
-    ImGui::SliderFloat("sun_disk[1]", &skybox::sun_disk[1], 0.000f, 0.100f, "%.03f");
     ImGui::SliderFloat("horizon[0]", &skybox::horizon[0], 0.000f, 0.250f, "%.03f");
     ImGui::SliderFloat("horizon[1]", &skybox::horizon[1], 0.000f, 0.250f, "%.03f");
 }
--- game/client/skybox.hh
+++ game/client/skybox.hh
@@ -6,14 +6,8 @@
 
 namespace skybox
 {
-extern Vec3f atmosphere;
-extern Vec2f sun_angles;
-extern Vec2f sun_disk;
 extern Vec2f horizon;
-} // namespace skybox
-
-namespace skybox
-{
+extern Vec2f sun_angles;
 extern Vec3f sun_direction;
 extern Vec3f fog_color;
 } // namespace skybox
