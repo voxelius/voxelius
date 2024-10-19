96341f2179a954ca11300cbf0b660c144e68b699
untodesu <41549137+untodesu@users.noreply.github.com>
2024-10-19 16:21:27 +0500

    Nuke skybox and fixup log texture tiling

--- game/client/skybox.hh
+++ game/client/skybox.hh
@@ -6,28 +6,10 @@
 
 namespace skybox
 {
-extern Vec3f absorption;
-extern float horizon_scale;
-extern float horizon_start;
-extern float horizon_end;
-extern float stars_gamma;
-extern unsigned int stars_scale;
-extern unsigned int stars_seed;
-extern float twilight_offset;
-extern Vec2f sun_angles;
-} // namespace skybox
-
-namespace skybox
-{
-extern Vec3f sun_direction;
 extern Vec3f fog_color;
 } // namespace skybox
 
 namespace skybox
 {
 void init(void);
-void deinit(void);
-void update(void);
-void render(void);
-void layout_debug(void);
 } // namespace skybox
